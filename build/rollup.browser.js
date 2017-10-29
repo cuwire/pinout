7b480a16f294f1c911425d62ae324c793fa5f14f
ivan baktsheev <dot.and.thing@gmail.com>
2017-10-29 19:17:03 +0300

    updated to newer rollup

--- build/rollup.browser.js
+++ build/rollup.browser.js
@@ -6,14 +6,19 @@ import alias from 'rollup-plugin-alias';
 
 import buble from 'rollup-plugin-buble';
 
-import {rollup} from 'rollup';
-import scss from 'rollup-plugin-scss';
+// import {rollup} from 'rollup';
+import scss     from 'rollup-plugin-scss';
+import ignore   from 'rollup-plugin-ignore';
 
 export default {
-	entry: 'src/index.js',
-	format: 'iife',
+	input: 'src/index.js',
+	
 	plugins: [
-		// buble()
+		ignore (['fs', 'net', 'util']),
+		scss ({
+			output: 'dist/pinout.css'
+		}),
+		buble (),
 		/*
 		alias({
 			jszip: path.join(__dirname, './node_modules/jszip/dist/jszip.min.js')
@@ -21,20 +26,20 @@ export default {
 		nodeResolve({ jsnext: true, module: true }),
 		commonjs()
 		*/
-		scss({
-			output: 'dist/pinout.css'
-		})
 	],
-	dest: 'dist/pinout.js',
+	output: {
+		file: 'dist/pinout.js',
+		format: 'iife',	
+	},
 	external: [
 		'xmldom',
 		'jszip',
 		'JSZipUtils',
 	],
-	sourceMap: true,
+	sourcemap: true,
 	globals: {
 		xmldom: 'window',
 		jszip: 'JSZip'
 	},
-	moduleName: 'Pinout'
+	name: 'Pinout'
 }
