cbb162a9e9929bb31feda4f74de112a36e705864
ivan baktsheev <dot.and.thing@gmail.com>
2017-11-03 13:06:00 +0300

    test: checking data

--- test/002-export-pinout.js
+++ test/002-export-pinout.js
@@ -1,6 +1,10 @@
+import FS from 'fs';
+
 import Fzpz from '../src/fritzing/fzpz';
 
 import SVGView from '../src/view/svg';
+
+import {DOMParser, XMLSerializer} from 'xmldom';
 
 describe ("002-export-pinout", function () {
 	it ("from fritzing", async function () {
@@ -12,10 +16,32 @@ describe ("002-export-pinout", function () {
 		console.log (Object.keys (fzpz.files.breadboard));
 		*/
 		
+		SVGView.css = FS.readFileSync ('./assets/pinout.sass');
+		
 		var pinoutView = new SVGView (fzpz);
 		
-		var pinoutSvg = pinoutView.serialize ();
+		/*{
 		
-		console.log (pinoutSvg);
+		var doc = pinoutView.document = pinoutView.parse ();
+		var root = doc.documentElement;
+
+		while (root.childNodes.length) {
+			root.removeChild (root.childNodes[root.childNodes.length - 1]);
+		}
+			
+		// console.log (pinoutSvg);
+			
+		}*/
+		
+		FS.writeFileSync (
+			'./test/002-JDY-08-breadboard.svg',
+			fzpz.files.breadboard.contents
+		);
+		
+		var pinoutSvg = pinoutView.export ();
+		
+		FS.writeFileSync ('./test/002-JDY-08-pinout.svg', pinoutSvg);
+		
+		// console.log (pinoutSvg);
 	});
 })
